cc346ced10d154bb73da24bb53f8506c364e4405
besoeasy <besoeasy@users.noreply.github.com>
2024-06-24 16:03:01 +0530

    chore: Refactor getGateway function and update nodes.json

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
-const axios = require('axios');
+const axios = require("axios");
+const gateways = require("./nodes.json");
 
-const gateways = require('./nodes.json');
+// Example hash for an image
+const imageHash = "QmR2S8BouhseEsHC5cc4KKtUobrWXkAEd48rqAKWayMiy5";
 
-async function axioscall(url) {
-	return await axios
-		.get(url, { timeout: 9000 })
-		.then(function (response) {
-			return response.data;
-		})
-		.catch(function () {
-			return false;
-		});
+async function axiosCall(url) {
+  try {
+    const response = await axios.get(url, { timeout: 3000 });
+    return response.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function checkImageSupport(gateway) {
+  try {
+    const url = `${gateway}${imageHash}`;
+    const response = await axios.get(url, { responseType: 'arraybuffer', timeout: 9000 });
+    const contentType = response.headers['content-type'];
+    return contentType && contentType.startsWith('image/');
+  } catch (error) {
+    return false;
+  }
 }
 
 async function getGateway() {
-	for (let i = gateways.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[gateways[i], gateways[j]] = [gateways[j], gateways[i]];
-	}
+  // Shuffle the gateways array using the Fisher-Yates algorithm
+  for (let i = gateways.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [gateways[i], gateways[j]] = [gateways[j], gateways[i]];
+  }
 
-	for (let i = 0; i < gateways.length; i++) {
-		try {
-			var gateway = gateways[i].trim();
+  for (const gateway of gateways) {
+    const trimmedGateway = gateway.trim();
 
-			var checkIPFSr = await axioscall(gateway + 'QmWj4nCkPT92VsqvybK4eDXcSVGY54FAURpTE9ryk5ahXa');
+    try {
+      const checkIPFSr = await axiosCall(`${trimmedGateway}QmXXDGe9cCkdc4GJo1xfWhsE2B7pEUR8vBcUvtjh6vzZNB`);
 
-			if (checkIPFSr.server) {
-				console.log(`GOOD : ${gateway}`);
-				return gateway;
-			} else {
-				console.log(`BAD : ${gateway}`);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+      if (checkIPFSr && checkIPFSr.server) {
+        console.log(`GOOD : ${trimmedGateway}`);
+        const supportsImage = await checkImageSupport(trimmedGateway);
+        
+        if (supportsImage) {
+          console.log(`Supports image: ${trimmedGateway}`);
+          return trimmedGateway;
+        } else {
+          console.log(`Does not support image: ${trimmedGateway}`);
+        }
+      } else {
+        console.log(`BAD : ${trimmedGateway}`);
+      }
+    } catch (error) {
+      console.error(`Error with gateway ${trimmedGateway}: ${error.message}`);
+    }
+  }
 
-	return null;
+  return null;
 }
 
-exports.getGateway = getGateway;
+module.exports = { getGateway };
